2c587371659835d92ec18297b638d8cab6dca444
thecaptaincarrot <trevorlandersen@gmail.com>
2021-09-30 23:21:51 -0500

    I fixed the spritesheets and added upgrade chests

---
+++ World/Upgrades/UpgradeChest.gd
@@ -0,0 +1,26 @@
+extends AnimatedSprite
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func activate(): #Generic activate function. Can do anything
+	
+	pass
+
+
+func _on_PlayerDetectionZone_body_entered(body):
+	if body.is_in_group("Player"):
+		activate()
